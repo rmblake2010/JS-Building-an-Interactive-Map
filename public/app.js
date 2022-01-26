89942dc6e3dc33a599e5e3976f488f89e9171f68
Ryan Blake <xbungalowx@gmail.com>
2022-01-25 21:33:10 -0600

    added user location & triangle drawing

--- public/app.js
+++ public/app.js
@@ -1,19 +1,55 @@
-// create map
+
+async function getCoords() {
+    const pos =  await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [pos.coords.latitude, pos.coords.longitude] 
+}
+
+function renderMap(coords){
+    let latlngTriangle = [];
+
+    const myMap = L.map("map", {
+        center: [coords[0], coords[1]],
+        zoom: 12,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+    }).addTo(myMap)
+
+      myMap.on('click', onMapClick)
+
+    function onMapClick(e) {
+        latlngTriangle.push(e.latlng)
+        console.log(latlngTriangle)
+        const redPin = L.icon({
+            iconUrl: './assets/red-pin.png',
+            iconSize: [38, 38],
+            iconAnchor: [19, 38],
+            popupAnchor: [0, -38]
+        })
+        const redMarker = L.marker([e.latlng.lat, e.latlng.lng], {icon: redPin}).bindPopup(e.latlng.toString()).addTo(myMap)
+       
+        triangleDraw(latlngTriangle)
+    }
+
+    function triangleDraw(latlng) {
+        if(latlng.length === 3) {
+            L.polygon(latlng, {color: 'red'}).addTo(myMap)
+            latlng.length = 0
+
+        }else {
+            return false
+        }
+    }
+}
 
 
-// add openstreetmap tiles
 
-
-// create and main add geolocation marker
-
-
-// draw the 2nd arrondissement
-
-
-// create red pin marker
-
-
-// metro station markers
-
-
+window.onload = async () => {
+    const coords = await getCoords()
+    renderMap(coords)
+}
 
